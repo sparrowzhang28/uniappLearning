b8e998aad7ea12567bf034807240a20dd71493e8
jack.zhang <823098037@qq.com>
2022-10-21 14:36:49 +0800

    init commit for learning

---
+++ pages/ViewList/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		查看作业
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad(){
+			
+			const userName=uni.getStorageSync('user')
+			console.log('123',userName)
+			const userName1=getApp().globalData.user
+			console.log('123-2',userName1)
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
